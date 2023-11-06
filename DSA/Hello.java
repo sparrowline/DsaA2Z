4c537ef52a23e68040c826386ee00d6c89733672
Syedrizwan99 <syedrizwansat95@gmail.com>
2023-11-06 20:49:53 +0000

    setup the environment

--- DSA/Hello.java
+++ DSA/Hello.java
@@ -1,11 +1,19 @@
 package DSA;
 
+import java.util.Scanner;
+
 /**
  * hello
  */
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("Enter an intiger to print");
+
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println("hey"+a);
+        sc.close();
     }
 }
